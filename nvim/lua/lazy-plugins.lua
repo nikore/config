9e1266c1dd16ea0fb6110d58c5424cb58ca94a4f
Matt Christiansen <matt@nikore.net>
2024-06-21 20:54:39 +0000

    more nvim plugins and configure tokyonight skin which is easier to read

--- nvim/lua/lazy-plugins.lua
+++ nvim/lua/lazy-plugins.lua
@@ -45,8 +45,8 @@ require("lazy").setup({
 	require("kickstart.plugins.which-key"),
 
 	-- themes
-	-- require("themes.tokyonight"),
-	require("themes.dracula"),
+	require("themes.tokyonight"),
+	-- require("themes.dracula"),
 
 	-- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
 	--    This is the easiest way to modularize your config.
@@ -75,4 +75,3 @@ require("lazy").setup({
 		},
 	},
 })
-
