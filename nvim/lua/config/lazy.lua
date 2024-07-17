327b1d9f3fc70c40ff86b198f3b9efdf67a98e1f
Matt Christiansen <matt@nikore.net>
2024-07-17 02:52:42 +0000

    removing kickstart files in favor of using LazyVim so I can irronically be more lazy

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -22,27 +22,6 @@ require("lazy").setup({
 
     -- NOTE: This is all my custom plugins
     { import = "custom.plugins" },
-
-    -- NOTE: Like most OG neovim setups I also started with kickstart
-    require("kickstart.plugins.autopairs"),
-    require("kickstart.plugins.cmp"),
-    require("kickstart.plugins.conform"),
-    require("kickstart.plugins.debug"),
-    require("kickstart.plugins.gitsigns"),
-    require("kickstart.plugins.go"),
-    require("kickstart.plugins.indent_line"),
-    require("kickstart.plugins.lint"),
-    require("kickstart.plugins.lspconfig"),
-    require("kickstart.plugins.mini"),
-    require("kickstart.plugins.neo-tree"),
-    require("kickstart.plugins.telescope"),
-    require("kickstart.plugins.todo-comments"),
-    require("kickstart.plugins.treesitter"),
-    require("kickstart.plugins.which-key"),
-
-    -- themes
-    require("themes.tokyonight"),
-    -- require("themes.dracula"),
   },
   defaults = {
     lazy = false,
