adfd24fd854671e9c0e030db8f8db49251ade734
Matt Christiansen <matt@nikore.net>
2025-01-13 15:25:04 -0800

    adding ghostty to worklaptop

--- home-manager/laptop.nix
+++ home-manager/laptop.nix
@@ -15,6 +15,7 @@
     ./modules/alacritty.nix
     ./modules/brave.nix
     ./modules/desktop_packages.nix
+    ./modules/ghostty.nix
     ./modules/gtk.nix
     ./modules/mouse.nix
     ./modules/rofi.nix
