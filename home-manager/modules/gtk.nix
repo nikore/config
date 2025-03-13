9b622cbd7bfccce02ff99f387d60fef3d8d2a861
Matt Christiansen <matt@nikore.net>
2025-03-13 10:09:29 -0700

    Improve dark-mode and theme selection

--- home-manager/modules/gtk.nix
+++ home-manager/modules/gtk.nix
@@ -9,11 +9,30 @@
       name = "Dracula";
       package = pkgs.dracula-icon-theme;
     };
-    gtk3.extraConfig = {
-      gtk-application-prefer-dark-theme=1;
+    gtk3 = {
+      extraConfig.gtk-application-prefer-dark-theme = true;
     };
-    gtk4.extraConfig = {
-      gtk-application-prefer-dark-theme=1;
+    gtk4 = {
+      extraConfig.gtk-application-prefer-dark-theme = true;
     };
   };
+  dconf.settings = {
+    "org/gtk/settings/file-chooser" = {
+      sort-directories-first = true;
+    };
+    "org/gnome/desktop/interface" = {
+      gtk-theme = "Dracula";
+      color-scheme = "prefer-dark";
+    };
+  };
+  qt = {
+    enable = true;
+    platformTheme.name = "gtk";
+    style = {
+      name = "gtk2";
+      package = pkgs.dracula-theme;
+    };
+  };
+
+  home.sessionVariables.GTK_THEME = "Dracula";
 }
